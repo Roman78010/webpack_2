6bb8950eae4c159387704001c56a502c7f33ab80
Roman78010 <109733236+Roman78010@users.noreply.github.com>
2024-01-04 19:17:26 +0200

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+// document.body.innerHTML = 'I want to go home';
+
+console.log('test');
+
+// const img = require('./assets/1.png');
+
+// document.body.innerHTML = `
+//   <img src="${img}" alt="img">
+// `;
